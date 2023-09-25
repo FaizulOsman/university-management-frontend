781a8af7434347ab8dea8be95191ebd0d435bc7b
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-25 16:45:15 +0600

    update: dashboard sidebar updated

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -51,8 +51,27 @@ const Sidebar = () => {
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
+      width={280}
+      style={{
+        overflow: "auto",
+        height: "100vh",
+        position: "sticky",
+        left: "0",
+        top: "0",
+        bottom: "0",
+      }}
     >
-      <div className="demo-logo-vertical" />
+      <div
+        style={{
+          color: "white",
+          fontSize: "2rem",
+          textAlign: "center",
+          fontWeight: "bold",
+          marginBottom: "1rem",
+        }}
+      >
+        PH-University
+      </div>
       <Menu
         theme="dark"
         defaultSelectedKeys={["1"]}
