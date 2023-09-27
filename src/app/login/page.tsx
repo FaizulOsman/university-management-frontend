fb70103e3447e9b9907b444f669aa5410c95a800
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-27 13:19:08 +0600

    feat: login and access token functionality increment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
 import { SubmitHandler } from "react-hook-form";
+import { useUserLoginMutation } from "@/redux/api/authApi";
+import { storeUserInfo } from "@/services/auth.service";
 
 type FormValues = {
   id: string;
@@ -12,10 +14,15 @@ type FormValues = {
 };
 
 const LoginPage = () => {
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
+  const [userLogin] = useUserLoginMutation();
+
+  const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
     try {
-      console.log(data);
-    } catch (err) {}
+      const res = await userLogin({ ...data }).unwrap();
+      storeUserInfo({ accessToken: res?.data?.accessToken });
+    } catch (err) {
+      console.error(err);
+    }
   };
   return (
     <Row
