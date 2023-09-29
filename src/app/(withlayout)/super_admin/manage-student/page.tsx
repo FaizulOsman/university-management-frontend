db1f41d834f73cb1ae9925915d29752c27f1752a
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-29 19:30:34 +0600

    feat: create student page created

--- src/app/(withlayout)/super_admin/manage-student/page.tsx
+++ src/app/(withlayout)/super_admin/manage-student/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { getUserInfo } from "@/services/auth.service";
+import { Button } from "antd";
+import Link from "next/link";
+
 const ManageStudentsPage = () => {
+  const { role } = getUserInfo() as any;
   return (
     <div>
-      <h1>ManageStudentsPage</h1>
+      <UMBreadCrumb
+        items={[
+          {
+            label: `${role}`,
+            link: `/${role}`,
+          },
+        ]}
+      />
+      <h1>Student List</h1>
+      <Link href="/super_admin/manage-student/create">
+        <Button type="primary">Create</Button>
+      </Link>
     </div>
   );
 };
