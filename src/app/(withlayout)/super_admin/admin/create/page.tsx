1dec3906dfc92706520ea4155a524031f263f377
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-29 19:43:49 +0600

    feat: create page created for all super_admin pages

---
+++ src/app/(withlayout)/super_admin/admin/create/page.tsx
@@ -0,0 +1,9 @@
+const CreateAdminPage = () => {
+  return (
+    <div>
+      <h1>CreateAdminPage</h1>
+    </div>
+  );
+};
+
+export default CreateAdminPage;
---
+++ src/app/(withlayout)/super_admin/department/create/page.tsx
@@ -0,0 +1,9 @@
+const CreateDepartmentPage = () => {
+  return (
+    <div>
+      <h1>CreateDepartmentPage</h1>
+    </div>
+  );
+};
+
+export default CreateDepartmentPage;
