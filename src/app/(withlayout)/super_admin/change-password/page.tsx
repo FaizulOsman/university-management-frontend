0889855daef6d1e9f5c5f4c9deef64c2d5481a21
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-29 19:21:19 +0600

    feat: folder and file created for super_admin

---
+++ src/app/(withlayout)/super_admin/change-password/page.tsx
@@ -0,0 +1,9 @@
+const ResetPassPage = () => {
+  return (
+    <div>
+      <h1>ResetPassPage</h1>
+    </div>
+  );
+};
+
+export default ResetPassPage;
---
+++ src/app/(withlayout)/super_admin/manage-student/page.tsx
@@ -0,0 +1,9 @@
+const ManageStudentsPage = () => {
+  return (
+    <div>
+      <h1>ManageStudentsPage</h1>
+    </div>
+  );
+};
+
+export default ManageStudentsPage;
